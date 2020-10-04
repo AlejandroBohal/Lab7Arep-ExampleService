b68578a4151f5667d49143daf4e758baee109909
AlejandroBohal <sergio.bohorquez@mail.escuelaing.edu.co>
2020-10-04 15:37:13 -0500

    javadoc

--- src/main/java/edu/escuelaing/arep/securehost/SecureHost.java
+++ src/main/java/edu/escuelaing/arep/securehost/SecureHost.java
@@ -2,12 +2,26 @@ package edu.escuelaing.arep.securehost;
 
 import static spark.Spark.*;
 
+/**
+ * The type Secure host.
+ */
 public class SecureHost {
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
     public static void main(String[] args) {
         secure("keystores/ecikeystore.p12", "pansito", "keystores/myTrustStoreLogin", "pansito");
         port(getPort());
         get("/hello", (req, res) -> "CONECCION CON OTRA API :D");
     }
+
+    /**
+     * Gets port.
+     *
+     * @return the port
+     */
     static int getPort() {
         if (System.getenv("PORT") != null) {
             return Integer.parseInt(System.getenv("PORT"));
